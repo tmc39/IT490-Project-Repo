f7d574142e8587975bd99d9eda4919f4d381ae6a
Mushran Chowdhury <mc2289@njit.edu>
2026-03-01 22:12:46 -0500

    Working authentication system: frontend login integrated with RabbitMQ and MySQL session handling

--- frontend/lib/rabbitMQ_web_client.php
+++ frontend/lib/rabbitMQ_web_client.php
@@ -3,35 +3,36 @@
 -----------------------
 rabbitMQ_web_client.php
 -----------------------
-This file is used by our web pages (like login.php and register.php)
-to talk to RabbitMQ.
 
-Why did we NOT use rabbitMQClient.php directly?
-    1) Because that file was written to be run from the terminal. 
-    2) It expects command line input and prints results to the screen.
-        NOTE: in the RabbitMQ video tutorial, we used ./testRabbitMQClient.php "Hey this is a variable message here" to  send a message to the testRabbitMQServer.php terminal program. This is a great way to test RabbitMQ, but it’s not how we want to build our web app.
+This is a small helper file for the web pages (login.php, home.php, register.php).
 
-In our web app:
-    1) There is no $argv (arguments from the command line)
-    2) Printing directly can break redirects
-    3) We want something simple and reusable
-        NOTE:So instead of changing the professor’s file, we created this small helper file just for the WEB SERVER side. This keeps things clean and easier to understand later.
+Important note:
+1) We only pass the INI filename (example: "testRabbitMQ.ini").
+2) Our get_host_info.inc already knows to load config files from integration/config/.
 */
 
-// Load the main RabbitMQ library.
-// NOTE: '__DIR__' a magic constant, makes sure PHP always knows where this file is. Otherwise, PHP only checks the current working directory.
-require_once __DIR__ . '/rabbitMQLib.inc';
-
-// This function sends a request to RabbitMQ and waits for a response from the backend.
-// We keep it simple, pass in an array and get back an array.
-function sendToRabbitMQ($request)
+function sendToRabbitMQ(array $request)
 {
-    // select the "gsServer" section name inside rabbitMQ.ini
-    $client = new rabbitMQClient(__DIR__ . "/rabbitMQ.ini", "gsServer");
+    // Point PHP to the integration/lib folder so rabbitMQLib.inc can find its helpers.
+    $integrationLib = realpath(__DIR__ . '/../../integration/lib');
+    if ($integrationLib === false) {
+        throw new Exception("Could not find integration/lib folder on this machine.");
+    }
 
-    // send_request waits for the backend to reply
+    set_include_path(get_include_path() . PATH_SEPARATOR . $integrationLib);
+
+    // Load the RabbitMQ library (it will pull in get_host_info.inc internally)
+    require_once 'rabbitMQLib.inc';
+
+    // We are using testServer for development right now
+    $iniFile = "testRabbitMQ.ini";
+    $serverSection = "testServer";
+
+    // Create RabbitMQ client and send request
+    $client = new rabbitMQClient($iniFile, $serverSection);
+
+    // send_request waits for a reply
     $response = $client->send_request($request);
 
     return $response;
 }
-?>
