f290e072583c5a7b36110796d1487e5d833bfb14
Mushran Chowdhury <mc2289@njit.edu>
2026-03-07 14:06:21 -0500

    Added 'Roboto' font style via Google Fonts

--- frontend/login.php
+++ frontend/login.php
@@ -106,6 +106,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
     <!-- Shared site CSS -->
     <link rel="stylesheet" href="/public/css/style.css">
 
--- index.php
+++ index.php
@@ -16,6 +16,12 @@ Simple landing page for project.
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I don't know what to call this site</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Shared site CSS -->
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
