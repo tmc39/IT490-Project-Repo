5ceaa1aab177dd8e0dd219c1d17563813035df01
MusaSShaikh <mss6@njit.edu>
2026-05-10 12:13:44 -0400

    removed API key, its in BigFatKeys

--- backend/BigFatKeys.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$fatSecretKey = "ef8da9a9d2014572811c4a18c59dc79a";
-$fatSecretSecret = "aff6cdac198348b699924c1dbf8c8022";
-$clarifaiPAT = "e435368a3c6d46a191646535c85be23f";
-$clarifaiUserID = "ekrt2e8rh5j5";
-$clarifaiAppID = "ekrt2e8rh5j5";
-
-?>
